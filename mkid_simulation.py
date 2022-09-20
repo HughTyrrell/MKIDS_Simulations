c75ad899478bd0fa4dbb428fb68ecf4187b32389
Hugh Tyrrell <ht.htyrrell@gmail.com>
2022-09-20 17:33:44 +0100

    Created data generating functions, created save function

--- mkid_simulation.py
+++ mkid_simulation.py
@@ -7,16 +7,32 @@ Created on Mon Sep 19 12:50:57 2022
 """
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
+import pandas as pd
 
+
+def pulse_func(x, a = 10, b = 32, c = 1):
+    # initialise amplitude, mean, standard deviation
+    
+    # define mathematical gaussian function
+    return a*np.exp((-(x-b)**2)/(2*c**2))
+        
+
+def noise_func(x, low = 4, high = 6):
+    
+    # create list of random numbers
+    # for each time interval within bounds
+    noise = np.random.uniform(low,high,len(x))
+    return noise
 
 
 def generate_data():
     # create the time series against which the data is plotted
-    time_data = [0,1,2,3,4,5,6,7,8,9,10]
+    time_data = np.arange(0,50)
     
     # create the signal data
-    pulse_data = [0,0,0,0,0,0,0,2,7,1,0]
-    noise_data = [3,3,2,3,3,4,3,2,3,3,4]
+    pulse_data = pulse_func(time_data)
+    noise_data = noise_func(time_data)
     
     # combine signal and noise
     data = np.add(pulse_data, noise_data)
@@ -35,8 +51,10 @@ def plot_data(time_data, data):
     plt.xlabel("Time")
     
     # sets reasonable limits
-    plt.ylim(0,15)
-    plt.xlim(0,12)
+    plt.ylim(0,20)
+    plt.xlim(0,50)
+    
+    #saves plot
     
     # displays the plot
     plt.show()
@@ -51,8 +69,24 @@ def main():
     print("Signal = ", data)
     
     # plots the data
+    plt.savefig('pulse_plot.png', dpi = 300)
     plot_data(time_data, data)
-
+    
+    #saving data to csv file
+    dict = {'Time': time_data, 'Signal': data}  
+       
+    df = pd.DataFrame(dict) 
+        
+    # saving the dataframe 
+    df.to_csv('pulse_sim.csv')
+    
+ 
 # this syntax is used to allow functions to be imported
 if __name__ == "__main__":
     main()
+    
+
+
+
+
+
